42cae4ad68d872a79e8f14189ac2a38651595f9b
Reza Merzaie <ghrezam97@gmail.com>
2023-08-29 12:35:42 +0430

    Add teacher class

---
+++ classes/teacher.rb
@@ -0,0 +1,12 @@
+require_relative 'person'
+
+class Teacher < Person
+  def initialize(age, name, parent_permission, specialization)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
