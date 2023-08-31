933d4b7c3c2e2531c4812cc35429d7e9d099dce0
Reza Merzaie <ghrezam97@gmail.com>
2023-08-31 16:30:08 +0430

    Create the has-many/belongs-to relationship between Classroom and Student

--- classes/student.rb
+++ classes/student.rb
@@ -4,6 +4,7 @@ class Student < Person
   def initialize(age, classroom, name: 'unknown', parent_permession: true)
     super(age, name, parent_permession)
     @classroom = classroom
+    @classroom.add_student(self)
   end
 
   def play_hooky
