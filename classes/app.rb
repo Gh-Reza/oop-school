0375226d5da11343bcfcbd6170381b2b26f51368
Reza Merzaie <ghrezam97@gmail.com>
2023-09-01 22:03:20 +0430

    Created the add_person, create_stuent and create teacher methods

--- classes/app.rb
+++ classes/app.rb
@@ -11,4 +11,41 @@ class App
   def list_people
     @people.each {|person| puts "[#{person.class}] Name: #{person.name}, ID: {person.id}, Age: #{person.age}"}
   end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp.downcase == 'y'
+    print 'Classroom: '
+    classroom = gets.chomp
+    Student.new(age, classroom, name, parent_permission)
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    Teacher.new(age, specialization, name)
+  end
+
+  def add_person(person)
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    person_type = gets.chomp.to_i
+
+    if person_type == 1
+      @people << create_student
+      puts 'Student created successfully'
+    elsif person_type == 2
+      @people << create_teacher
+      puts 'Teacher created successfully'
+    else
+      puts 'Invalid option'
+    end
+  end
 end
